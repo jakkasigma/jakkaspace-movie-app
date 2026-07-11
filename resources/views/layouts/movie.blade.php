24f15672bf187ecbffecbac17498587d4442b82e
virgi <akunkuliahvirgi@gmail.com>
2026-07-11 12:07:31 +0700

    fix: space-tab-bar jadi inline horizontal scroll (mobile), rollback bottom-nav overlap

--- resources/views/layouts/movie.blade.php
+++ resources/views/layouts/movie.blade.php
@@ -58,7 +58,7 @@
 </head>
 <body class="@yield('body-class')">
     @yield('body')
-    @unless(request()->is('login', 'register', 'forgot-password', 'reset-password', 'verify-email', 'confirm-password', 'inbox/*', 'your-space*'))
+    @unless(request()->is('login', 'register', 'forgot-password', 'reset-password', 'verify-email', 'confirm-password', 'inbox/*'))
         <x-bottom-nav />
     @endunless
 </body>
